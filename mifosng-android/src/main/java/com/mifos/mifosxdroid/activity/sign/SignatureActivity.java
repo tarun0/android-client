18f5e2a6dbde91405cb09df18db43c63c47710f5
Tarun Mudgal <tarun.mudgal34@gmail.com>
2017-06-21 13:59:57 +0530

    sign initial commit

---
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/activity/sign/SignatureActivity.java
@@ -0,0 +1,15 @@
+package com.mifos.mifosxdroid.activity.sign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mifos.mifosxdroid.R;
+
+public class SignatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signature);
+    }
+}
